ba3dbde4ad81babbf7ee64ae1d37e20afa795691
SUPRIYA <hnsupriya214@gmail.com>
2024-08-03 21:37:50 -0400

    02/08/2024 commit

--- src/com/course1/FileReadingAndWriting.java
+++ src/com/course1/FileReadingAndWriting.java
@@ -1,13 +1,33 @@
 package com.course1;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class FileReadingAndWriting {
 	public static void main(String[] args) {
 		File f1 = new File("Users/nandi/OneDrive/Desktop/test1.txt/test1.txt");
-		System.out.println(f1.getAbsolutePath());
+				System.out.println(f1.getAbsoluteFile());
+
+				FileInputStream fis;
+				try {
+					fis = new FileInputStream(f1);
+
+					InputStreamReader isreader = new InputStreamReader(fis);
+
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				} catch (IOException exp) {
+					exp.printStackTrace();
+				}
+
+			}
+
 		
 		
 	}
 
-}
+
